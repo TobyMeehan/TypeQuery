b441ef0c29a5ecde26d2d59e768a5414d1add3f7
Toby <toby@tobymeehan.com>
2020-08-19 22:46:33 +0100

    Add sql parameter clause
    Added sql clause type for parameters

---
+++ TypeQuery/ParameterSqlClause.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeQuery.Collections;
+
+namespace TypeQuery
+{
+    public class ParameterSqlClause : SqlClause
+    {
+        private readonly object _value;
+        private readonly string _key;
+
+        public ParameterSqlClause(object value)
+        {
+            _value = value;
+            _key = Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+        public override string ToString()
+        {
+            return $"@{_key}";
+        }
+
+        public override string ToString(out ParameterDictionary parameters)
+        {
+            parameters = new ParameterDictionary
+            {
+                { _key, _value }
+            };
+
+            return ToString();
+        }
+    }
+}
