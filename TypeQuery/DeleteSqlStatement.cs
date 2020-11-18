4dca7f99d5e334aec2b21d593f550d76a11d570f
Toby Meehan <toby@tobymeehan.com>
2020-11-18 09:28:16 +0000

    Add delete statement
    Added delete sql statement

---
+++ TypeQuery/DeleteSqlStatement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeQuery.Extensions;
+
+namespace TypeQuery
+{
+    public class DeleteSqlStatement : SqlStatement
+    {
+        public DeleteSqlStatement(Type tableType)
+        {
+            Clauses.Add(new RawSqlClause($"DELETE FROM {tableType.GetTableName()}"));
+        }
+    }
+}
---
+++ TypeQuery/SqlQuery.Delete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeQuery
+{
+    public partial class SqlQuery<T>
+    {
+        private DeleteSqlStatement SetDelete()
+        {
+            if (!(GetStatement() is DeleteSqlStatement deleteStatement))
+            {
+                deleteStatement = new DeleteSqlStatement(typeof(T));
+                SetStatement(deleteStatement);
+            }
+
+            return deleteStatement;
+        }
+
+        private SqlQuery<T> AddDelete()
+        {
+            SetDelete();
+
+            return this;
+        }
+
+        public SqlQuery<T> Delete()
+        {
+            return CloneQuery().AddDelete();
+        }
+    }
+}
