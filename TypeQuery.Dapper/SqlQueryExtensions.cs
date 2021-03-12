15531d75efce12b000d900d14fbe73e2dd955932
Toby Meehan <toby@tobymeehan.com>
2021-03-12 11:03:40 +0000

    Add dapper parameter extension
    Added extension to SqlQuery converting from dictionary to dynamic parameters.

---
+++ TypeQuery.Dapper/SqlQueryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TypeQuery.Collections;
+
+namespace TypeQuery.Dapper
+{
+    public static class SqlQueryExtensions
+    {
+        public static string ToString<T>(this SqlQuery<T> query, out object parameters)
+        {
+            string result = query.ToString(out ParameterDictionary dict);
+
+            parameters = new DynamicParameters(dict);
+
+            return result;
+        }
+    }
+}
