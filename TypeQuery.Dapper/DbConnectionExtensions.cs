71af5135e90e3e74e96fc68bf2f595e4fa6de1ba
Toby Meehan <toby@tobymeehan.com>
2021-03-15 09:11:01 +0000

    Add dbconnection extensions
    Added extensions to IDbConnection interface for easy dapper interaction

---
+++ TypeQuery.Dapper/DbConnectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeQuery.Dapper
+{
+    public static class DbConnectionExtensions
+    {
+        public static IEnumerable<T> Query<T>(this IDbConnection connection, SqlQuery<T> query, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.Query<T>(query.ToString(out object parameters), parameters, transaction, buffered, commandTimeout, commandType);
+        }
+        public static IEnumerable<T> Query<T>(this IDbConnection connection, SqlQuery<T> query, Type[] types, Func<object[], T> map, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.Query(query.ToString(out object parameters), types, map, parameters, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static Task<IEnumerable<T>> QueryAsync<T>(this IDbConnection connection, SqlQuery<T> query, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryAsync<T>(query.ToString(out object parameters), parameters, transaction, commandTimeout, commandType);
+        }
+        public static Task<IEnumerable<T>> QueryAsync<T>(this IDbConnection connection, SqlQuery<T> query, Type[] types, Func<object[], T> map, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.QueryAsync(query.ToString(out object parameters), types, map, parameters, transaction, buffered, splitOn, commandTimeout, commandType);
+        }
+
+        public static int Execute<T>(this IDbConnection connection, SqlQuery<T> query, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.Execute(query.ToString(out object parameters), parameters, transaction, commandTimeout, commandType);
+        }
+        public static Task<int> ExecuteAsync<T>(this IDbConnection connection, SqlQuery<T> query, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return connection.ExecuteAsync(query.ToString(out object parameters), parameters, transaction, commandTimeout, commandType);
+        }
+    }
+}
